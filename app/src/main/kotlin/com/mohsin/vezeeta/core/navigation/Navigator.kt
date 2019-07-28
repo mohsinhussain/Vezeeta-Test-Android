e0765595428145e223163066e5207dc854f6f77a
Mohsin Hussain <m.hussain@tatweer-co.ae>
2019-07-28 18:30:12 +0400

    Refacting 2

--- app/src/main/kotlin/com/mohsin/vezeeta/core/navigation/Navigator.kt
+++ app/src/main/kotlin/com/mohsin/vezeeta/core/navigation/Navigator.kt
@@ -15,35 +15,32 @@
  */
 package com.mohsin.vezeeta.core.navigation
 
-import android.content.ActivityNotFoundException
+
 import android.content.Context
-import android.content.Intent
-import android.net.Uri
 import androidx.core.app.ActivityOptionsCompat
 import androidx.fragment.app.FragmentActivity
 import android.view.View
 import android.widget.ImageView
-import com.mohsin.vezeeta.features.login.Authenticator
-import com.mohsin.vezeeta.features.login.LoginActivity
 import com.mohsin.vezeeta.features.characters.CharacterDetailsActivity
 import com.mohsin.vezeeta.features.characters.CharacterView
 import com.mohsin.vezeeta.features.characters.CharactersActivity
-import com.mohsin.vezeeta.core.extension.empty
 import javax.inject.Inject
 import javax.inject.Singleton
 
 
 @Singleton
 class Navigator
-@Inject constructor(private val authenticator: Authenticator) {
+@Inject constructor()
+  {
 
-    private fun showLogin(context: Context) = context.startActivity(LoginActivity.callingIntent(context))
+//    private fun showLogin(context: Context) = context.startActivity(LoginActivity.callingIntent(context))
 
     fun showMain(context: Context) {
-        when (authenticator.userLoggedIn()) {
-            true -> showMovies(context)
-            false -> showLogin(context)
-        }
+        showMovies(context)
+//        when (authenticator.userLoggedIn()) {
+//            true ->
+//            false -> showLogin(context)
+//        }
     }
 
     private fun showMovies(context: Context) = context.startActivity(CharactersActivity.callingIntent(context))
@@ -56,32 +53,32 @@ class Navigator
         activity.startActivity(intent, activityOptions.toBundle())
     }
 
-    private val VIDEO_URL_HTTP = "http://www.youtube.com/watch?v="
-    private val VIDEO_URL_HTTPS = "https://www.youtube.com/watch?v="
+//    private val VIDEO_URL_HTTP = "http://www.youtube.com/watch?v="
+//    private val VIDEO_URL_HTTPS = "https://www.youtube.com/watch?v="
 
-    fun openVideo(context: Context, videoUrl: String) {
-        try {
-            context.startActivity(createYoutubeIntent(videoUrl))
-        } catch (ex: ActivityNotFoundException) {
-            context.startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(videoUrl)))
-        }
-    }
-
-    private fun createYoutubeIntent(videoUrl: String): Intent {
-        val videoId = when {
-            videoUrl.startsWith(VIDEO_URL_HTTP) -> videoUrl.replace(VIDEO_URL_HTTP, String.empty())
-            videoUrl.startsWith(VIDEO_URL_HTTPS) -> videoUrl.replace(VIDEO_URL_HTTPS, String.empty())
-            else -> videoUrl
-        }
-
-        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("vnd.youtube:$videoId"))
-        intent.putExtra("force_fullscreen", true)
-
-        if (android.os.Build.VERSION.SDK_INT <= android.os.Build.VERSION_CODES.M)
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-
-        return intent
-    }
+//    fun openVideo(context: Context, videoUrl: String) {
+//        try {
+//            context.startActivity(createYoutubeIntent(videoUrl))
+//        } catch (ex: ActivityNotFoundException) {
+//            context.startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(videoUrl)))
+//        }
+//    }
+//
+//    private fun createYoutubeIntent(videoUrl: String): Intent {
+//        val videoId = when {
+//            videoUrl.startsWith(VIDEO_URL_HTTP) -> videoUrl.replace(VIDEO_URL_HTTP, String.empty())
+//            videoUrl.startsWith(VIDEO_URL_HTTPS) -> videoUrl.replace(VIDEO_URL_HTTPS, String.empty())
+//            else -> videoUrl
+//        }
+//
+//        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("vnd.youtube:$videoId"))
+//        intent.putExtra("force_fullscreen", true)
+//
+//        if (android.os.Build.VERSION.SDK_INT <= android.os.Build.VERSION_CODES.M)
+//            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+//
+//        return intent
+//    }
 
     class Extras(val transitionSharedElement: View)
 }
